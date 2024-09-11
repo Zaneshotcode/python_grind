fb1bc752e031839c0de9e5200e1cf76287320f2a
xing <yxh418772301@me.com>
2024-09-10 21:59:44 -0400

    random function

---
+++ randomization.py
@@ -0,0 +1,18 @@
+import random
+
+
+# random_integer = random.randint(1,10)
+# print(random_integer)
+
+# random_number_0_to_1 = random.random()
+# print(random_number_0_to_1)
+
+# random_float = random.uniform(0,10)
+# print(random_float)
+
+user_input = random.randint(1,10)
+if user_input % 2 == 1:
+    print("this is a odd number , heads")
+else:
+    print("This is a even number ,tails")
+
